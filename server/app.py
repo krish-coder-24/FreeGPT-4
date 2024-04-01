91274b4abd0a0b2f4db7e8b7892e07558e204c47
Krish Anjara <127621879+krish-coder-24@users.noreply.github.com>
2024-04-02 00:31:33 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -2,5 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__, template_folder='./../client/html')
+app.config['DEBUG'] = True
 CORS(app, origins=['https://626ef8539c2a85c784499fd724765f22.serveo.net'])
 
