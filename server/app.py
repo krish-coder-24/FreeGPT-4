459e4ea78fc50b39ed8925af1aba6ec025fc1e7f
Krish Anjara <127621879+krish-coder-24@users.noreply.github.com>
2024-09-10 03:05:28 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,7 +1,4 @@
 from flask import Flask
-from flask_cors import CORS
 
 app = Flask(__name__, template_folder='./../client/html')
 app.config['DEBUG'] = True
-CORS(app, origins=['http://localhost:8000'])
-
